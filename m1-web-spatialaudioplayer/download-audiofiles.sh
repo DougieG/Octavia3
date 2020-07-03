81cc70f834fbc1d48001c8e3b1ed0d0e2208a974
DougieG <55158561+DougieG@users.noreply.github.com>
2020-07-03 02:46:58 -0400

    Update download-audiofiles.sh

--- m1-web-spatialaudioplayer/download-audiofiles.sh
+++ m1-web-spatialaudioplayer/download-audiofiles.sh
@@ -17,11 +17,12 @@ fi
 mkdir -p audio/m1spatial
 
 cd ../m1spatial
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T1.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T2.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T3.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T4.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B5.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B6.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B7.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B8.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.T1.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.T2.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.T3.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.T4.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.B5.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.B6.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.B7.ogg
+wget https://dougieg.github.io/Octavia3/m1-web-spatialaudioplayer/audio/m1spatial/Saloon.B8.ogg
+
